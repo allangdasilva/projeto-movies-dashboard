1bb011296611632c5dacd799f7261c8f5fff7770
Allan Silva <allandasilva33@outlook.com>
2025-01-18 15:00:59 -0300

    Estilização MovieCard completa

--- src/components/Movie/MovieCard.jsx
+++ src/components/Movie/MovieCard.jsx
@@ -25,33 +25,37 @@ function MovieCard(){
     function getMovieVideo(){
         axios.get(`${moviesURL}${id}/videos?language=pt-BR&${apiKey}`)
         .then(response => {
-            setMovieVideo(response.data.results)
+            setMovieVideo(response.data.results[0])
         })
         .catch(error => console.log(error))
     }
 
     return (
 
-        <section className="w-full max-w-screen-md min-h-screen flex flex-col gap-4 justify-center p-8">
-            <h2>
-                <button onClick={()=> navigate(-1)} className="flex flex-wrap items-center gap-2 max-w-max break-all text-lg text-white">
-                    <ArrowBigLeft color="#F9F9F9" /> <Popcorn color="#79D7BE" size={28} /> MoviesDash 
-                </button>
-            </h2>
-            <h2 className="text-4xl font-bold break-words text-white">{movie.title}</h2>
-            <ul className="flex flex-wrap items-center gap-2">
-              <li className="px-2 text-white shadow-md bg-gradient-to-t from-amber-500 to-yellow-400">
-                {movie.vote_average && movie.vote_average.toFixed(1)}
-              </li>
-              <li className="text-white">{movie.release_date && movie.release_date.substr(0,4)}</li>
-              <li className="text-white">{movie.runtime} Min</li>
-            </ul>
-            <p className="text-gray-300">{movie.overview}</p>
-            <p className="text-white">{movie.genres && movie.genres.map((genre) => genre.name).join(", ")}</p>
+        <section className="w-full min-h-screen flex flex-col gap-8 items-center justify-center p-8 lg:flex-row">
+            <div className="w-full flex flex-col gap-4 justify-center">
+                <h2>
+                    <button onClick={()=> navigate(-1)} className="flex flex-wrap items-center gap-2 max-w-max break-all text-lg text-white">
+                        <ArrowBigLeft color="#F9F9F9" /> <Popcorn color="#79D7BE" size={28} /> MoviesDash 
+                    </button>
+                </h2>
+                <h2 className="text-4xl font-bold break-words text-white">{movie.title}</h2>
+                <ul className="flex flex-wrap items-center gap-2">
+                <li className="px-2 text-white shadow-md bg-gradient-to-t from-amber-500 to-yellow-400">
+                    {movie.vote_average && movie.vote_average.toFixed(1)}
+                </li>
+                <li className="text-white">{movie.release_date && movie.release_date.substr(0,4)}</li>
+                <li className="text-white">{movie.runtime} Min</li>
+                </ul>
+                <p className="text-gray-300">{movie.overview}</p>
+                <p className="text-white">{movie.genres && movie.genres.map((genre) => genre.name).join(", ")}</p>
+            </div>
+            
 
-            <div className="text-white">{movieVideo.map((ele)=>(
-                <iframe width="560" height="315" src={`https://www.youtube.com/embed/${ele.key}`} title={`${ele.name}`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen key={ele.id}></iframe>
-            ))}</div>
+            <div className="w-full flex justify-center text-white">{movieVideo && (
+                <iframe width="560" height="315" src={`https://www.youtube.com/embed/${movieVideo.key}`} title={`${movieVideo.name}`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen
+                className="w-full max-w-lg max-h-72 aspect-video shadow-md"></iframe>
+            )}</div>
 
             {
             isLoading && 
